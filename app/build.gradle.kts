a6e43423bf1b632d9b516e213d749aaaaed6d39a
Amadeus <criwbasct77@gmail.com>
2025-12-05 14:35:17 +0700

    COMMIT to GITHUB at this point. Commit Message: “Commit No. 1 – Init template”.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.moshi.kotlin)
+    kapt(libs.moshi.kotlin.codegen)
 }
